0ad8efd4d17ac1e073bdb18c4dd98094b65d5345
Nick DiRienzo <nicholas.dirienzo@optimizely.com>
2018-08-08 15:00:53 -0700

    Rebase with upstream and apply changes

--- laboratory/__init__.py
+++ laboratory/__init__.py
@@ -1,6 +1,6 @@
 from .exceptions import LaboratoryException, MismatchException
 from .experiment import Experiment
 
-__version__ = '1.0'
+__version__ = '1.0.1'
 
 __all__ = ('Experiment', 'LaboratoryException', 'MismatchException')
