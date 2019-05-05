b8e81888b8ad509cef7ce1fef5e4defd10923668
Joe Alcorn <joealcorn123@gmail.com>
2019-05-05 16:00:22 +0100

    bump patch version

--- laboratory/__init__.py
+++ laboratory/__init__.py
@@ -1,6 +1,6 @@
 from .exceptions import LaboratoryException, MismatchException
 from .experiment import Experiment
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 __all__ = ('Experiment', 'LaboratoryException', 'MismatchException')
